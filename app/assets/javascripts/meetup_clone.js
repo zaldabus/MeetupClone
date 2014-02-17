65b971e6f95ac5b7a70ddaa09454cec1a887a36d
App Academy Student <student@appacademy.io>
2014-02-17 11:17:40 -0500

    Initial commit

---
+++ app/assets/javascripts/meetup_clone.js
@@ -0,0 +1,13 @@
+window.MeetupClone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+
+  }
+};
+
+$(document).ready(function(){
+  MeetupClone.initialize();
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetupClone::Application.config.secret_token = 'df605709c8e38ef7b9bd93ee188952d0257f0555ce2bf92d505a6a7fa382338b2dd0ab53759d327549b0ed0e4e3ff92d3470cbd64e9805d1e02c01287b497436'
