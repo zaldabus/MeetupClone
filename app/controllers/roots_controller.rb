d1d5ecfd47b1754f167250dff5445e4367514f97
Ryan Beckman <zaldabus@gmail.com>
2014-02-20 15:13:49 -0500

    User root pages shows all upcoming events from user's groups

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -1,5 +1,20 @@
 class RootsController < ApplicationController
   def index
 
+
+    if logged_in?
+      events = []
+      groups = current_user.groups.includes(:events).where("events.date BETWEEN ? AND ?", Time.now, (Time.now + 1.year) )
+      groups.each do |group|
+        events.concat(group.events)
+      end
+      @events = events.sort_by &:date
+    else
+      @groups = Group.all
+    end
+  end
+
+  def find
+    @groups = Group.all
   end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,7 +1,7 @@
 module GroupsHelper
   def time_since(date)
     if (Time.now - date).to_i / 1.day > 30
-      time = (Time.now - time).to_i / 1.month
+      time = (Time.now - date).to_i / 1.month
       months = "month".pluralize(time)
 
       "#{time} #{months} ago"
