0e091327ef32d8cec04c93535175fd55511096f6
Ryan Beckman <zaldabus@gmail.com>
2014-02-21 16:04:25 -0500

    removed directories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,9 +16,4 @@
 //= require underscore
 //= require backbone
 //= require meetup_clone
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
 //= require_tree .
