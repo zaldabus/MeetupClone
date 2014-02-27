aabc4b984bf0c6e089de36e889dd0b71af8725f1
Ryan Beckman <zaldabus@gmail.com>
2014-02-27 09:37:44 -0500

    User can change password

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -23,4 +23,27 @@ class AccountsController < ApplicationController
   def notifications
     @notifications = current_user.notifications
   end
+
+  def change_password
+    @user = current_user
+  end
+
+  def update_password
+    @user = current_user
+
+    if @user.has_password?(params[:user][:password])
+
+      if params[:user][:new_password] == params[:user][:password_confirmation]
+        @user.password = params[:user][:new_password]
+        flash[:notice] = "Password Updated!"
+        redirect_to account_url
+      else
+        flash.now[:errors] = "Please make sure new password and confirmation match!"
+        render :change_password
+      end
+    else
+      flash.now[:errors] = "Old Password Incorrect!"
+      render :change_password
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ MeetupClone::Application.routes.draw do
 
   resource :account, only: [:show, :edit, :update] do
     get 'change_password', on: :collection
+    put 'update_password', on: :collection
     get 'change_address', on: :collection
     get 'notifications', on: :collection
   end
